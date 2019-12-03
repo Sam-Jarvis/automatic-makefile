b01493dfb308ea41e9c8f5a8335f98397104b90d
Sam Jarvis <samjarvis.nam@gmail.com>
2019-12-03 11:30:48 +0100

    Modified, not yet working

--- install.py
+++ install.py
@@ -1,16 +1,19 @@
 import subprocess
 import os
-process = subprocess.Popen(["sudo chmod +x mf.py", "sudo mv mf.py mf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                           shell=True)
-stdout, stderr = process.communicate()
+# process = subprocess.Popen(["sudo chmod +x mf.py", "sudo mv mf.py mf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+#                            shell=True)
+# stdout, stderr = process.communicate()
 
 hm_dir = os.path.expanduser('~')
-os.mkdir(hm_dir + "/mf")
+#os.mkdir(hm_dir + "/mf")
 
-process2 = subprocess.Popen(["sudo mv mf {}".format(hm_dir + '/mf')], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                            shell=True)
-stdout, stderr = process2.communicate()
+process2 = subprocess.Popen(["sudo mv mf.py {}".format(hm_dir + '/mf')], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
 
-process3 = subprocess.Popen(["""echo 'export PATH=$PATH":$HOME/mf"' >> .profile"""], stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE, shell=True)
-stdout, stderr = process3.communicate()
+os.chdir(hm_dir)
+with open('.profile', 'a') as profile:
+    profile.write('PATH=$PATH":$HOME/mf"')
+
+#process3 = subprocess.Popen(['PATH=$PATH":$HOME/mf"'], stdout=subprocess.PIPE,
+#                             stderr=subprocess.PIPE, shell=True)
+# stdout, stderr = process3.communicate()
